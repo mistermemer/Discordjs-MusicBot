7538ebe588a36806539dba7856b0a2b6c801236b
mistermemer <77989086+mistermemer@users.noreply.github.com>
2021-06-23 14:33:39 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,17 +1,20 @@
-module.exports = (client, message) => {
-  if (message.author.bot) return;
+const { Database } = require("quickmongo");
+const db = new Database("mongodb+srv://GentleScreamer:Piku2007@cluster0.wofjo.mongodb.net/mythical?retryWrites=true&w=majority");  
 
-  if (message.content.indexOf(client.config.prefix) !== 0) return;
+module.exports = async (client, message) => {
+    if (message.author.bot || message.channel.type === 'dm') return;
+    
+      let prefixes = await db.fetch(`prefix_${message.guild.id}`)
+      if(!prefixes) prefixes = ['gg'];
+      
+let prefix = prefixes.find(p => message.content.toLowerCase().startsWith(p));
+if (!prefix) return;
+    
 
-  const args = message.content
-    .slice(client.config.prefix.length)
-    .trim()
-    .split(/ +/g);
-  const command = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-  const cmd = client.commands.get(command);
-
-  if (!cmd) return;
-
-  cmd.run(client, message, args);
+    const cmd = client.commands.get(command)
+    if (cmd) cmd.run(client, message, args);
 };
+

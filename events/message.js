93294111af09e074b95191582edced891a1ef891
mistermemer <77989086+mistermemer@users.noreply.github.com>
2022-08-23 09:52:21 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,12 +1,8 @@
-const { Database } = require("quickmongo");
-const db = new Database("mongodb+srv://GentleScreamer:Piku2007@cluster0.wofjo.mongodb.net/jelly?retryWrites=true&w=majority");  
 
 module.exports = async (client, message) => {
     if (message.author.bot || message.channel.type === 'dm') return;
     
-      let prefixes = await db.fetch(`prefix_${message.guild.id}`)
-      if(!prefixes) prefixes = ['gg'];
-      
+      let prefixes = ['dj']
 let prefix = prefixes.find(p => message.content.toLowerCase().startsWith(p));
 if (!prefix) return;
     
